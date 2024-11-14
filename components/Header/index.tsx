7b2e4693db9cf9ebd3948abd0fca0ca2048ea7e2
Mheyrie <omota4fidelity@gmail.com>
2024-11-14 15:39:16 +0100

    metadate update and imgae upt

--- components/Header/index.tsx
+++ components/Header/index.tsx
@@ -49,20 +49,20 @@ const Header = () => {
       >
         <div className="container">
           <div className="relative -mx-4 flex items-center justify-between">
-            <div className="w-60 max-w-full px-4 xl:mr-12">
+            <div className="w-40 max-w-full px-4 xl:mr-12">
               <Link
                 href="/"
                 className={`header-logo block w-full ${
                   sticky ? "py-5 lg:py-2" : "py-8"
                 } `}
               >
-                <Image
+                {/* <Image
                   src="/images/logo/logo-2.svg"
                   alt="logo"
                   width={140}
                   height={30}
                   className="w-full dark:hidden"
-                />
+                /> */}
                 <Image
                   src="/images/logo/logo.svg"
                   alt="logo"
