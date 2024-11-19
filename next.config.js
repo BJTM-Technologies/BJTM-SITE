ef1bcb05b78caf651db77c377f7b799f53d8927c
bamidele efunnuga <bamideleefunnuga9@gmail.com>
2024-11-19 06:33:25 +0000

    feat:Mod

--- next.config.js
+++ next.config.js
@@ -1,6 +1,7 @@
 /** @type {import('next').NextConfig} */
 require('dotenv').config();
 const nextConfig = {
+  trailingSlash: true,
   output: "export",
   images: {
     unoptimized: true, // Disable image optimization for static exports
